38a3445aa247a27d8b4e86417608ed1f882f2364
DJCC <david.carcamo2006@gmail.com>
2025-07-24 11:41:16 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author David
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Aqui hay una calculadora de prueba");
+    }
+}
